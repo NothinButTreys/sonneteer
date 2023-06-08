3874cad0107186de816b1efff33573269be888e0
NutnButTreys <tom.fmcbride@gmail.com>
2023-06-07 20:18:00 -0700

    fixes to get it working again

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
-require('dotenv').config();
+require("dotenv").config();
 //DiscordJS
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const Discord = require('discord.js')
+const client = new Discord.Client({
+  intents: [
+    Discord.GatewayIntentBits.Guilds,
+    Discord.GatewayIntentBits.GuildMessages,
+    Discord.GatewayIntentBits.GuildVoiceStates,
+    Discord.GatewayIntentBits.MessageContent
+  ]
+});
 
 // Create a new DisTube
-const DisTube = require('distube');
+const { DisTube } = require('distube');
+const { SpotifyPlugin } = require('@distube/spotify');
+const { SoundCloudPlugin } = require('@distube/soundcloud');
+const { YtDlpPlugin } = require('@distube/yt-dlp');
 const distube = new DisTube(client, {
-  searchSongs: true,
+  leaveOnStop: false,
   emitNewSongOnly: true,
-  highWaterMark: 1 << 25,
+  emitAddSongWhenCreatingQueue: false,
+  emitAddListWhenCreatingQueue: false,
+  plugins: [
+    new SpotifyPlugin({
+      emitEventsAfterFetching: true
+    }),
+    new SoundCloudPlugin(),
+    new YtDlpPlugin()
+  ]
 });
 
 client.on('ready', () => {
@@ -29,11 +47,11 @@ client.on('message', async message => {
       return message.channel.send('You are not in a voice channel.');
     }
 
-    distube.play(message.member.voice.channel, args.join(" "), {
+    distube.playVoiceChannel(message.member.voice.channel, message.options.song, {
       member: message.member,
       textChannel: message.channel,
       message
-  });
+    });
   }
 
   if (command == 'stop') {
@@ -58,26 +76,24 @@ client.on('message', async message => {
 
   if (command == 'autoplay') {
     let mode = distube.toggleAutoplay(message);
-    message.channel.send('Set autoplay mode to `' + (mode ? 'On' : 'Off') + '`');
+    const messageToSend = 'Set autoplay mode to `' + (mode ? 'On' : 'Off') + '`';
+    return message.channel.send(messageToSend);
   }
 
   if (['loop', 'repeat'].includes(command)) {
     let mode = distube.setRepeatMode(message, parseInt(args[0]));
     mode = mode ? (mode == 2 ? 'Repeat queue' : 'Repeat song') : 'Off';
-    message.channel.send('Set repeat mode to `' + mode + '`');
+    const messageToSend = 'Set repeat mode to `' + mode + '`';
+    return message.channel.send(messageToSend);
   }
 
   if (command == 'queue') {
     let queue = distube.getQueue(message);
-    message.channel.send(
-      'Current queue:\n' +
-        queue.songs
-          .map(
-            (song, id) =>
-              `**${id + 1}**. [${song.name}](${song.url}) - \`${song.formattedDuration}\``
-          )
-          .join('\n')
-    );
+    const messageToSend = 'Current queue:\n' + queue.songs.map((song, id) =>
+      `**${id + 1}**. [${song.name}](${song.url}) - \`${song.formattedDuration}\``
+    ).join('\n');
+
+    return message.channel.send(messageToSend);
   }
 });
 
@@ -89,51 +105,54 @@ const status = queue =>
 
 // DisTube event listeners
 distube
-  .on('playSong', (message, queue, song) =>
-    message.channel.send(
-      `Playing \`${song.name}\` - \`${song.formattedDuration}\`\nRequested by: ${song.user.tag}`
-    )
-  )
-  .on('addSong', (message, queue, song) =>
-    message.channel.send(
-      `Added ${song.name} - \`${song.formattedDuration}\` to the queue by ${song.user.tag}`
-    )
-  )
-  .on('playList', (message, queue, playlist, song) =>
-    message.channel.send(
-      `Play \`${playlist.name}\` playlist (${
-        playlist.songs.length
-      } songs).\nRequested by: ${song.user.tag}\nNow playing \`${song.name}\` - \`${
-        song.formattedDuration
-      }\`\n${status(queue)}`
-    )
-  )
-  .on('addList', (message, queue, playlist) =>
-    message.channel.send(
-      `Added \`${playlist.name}\` playlist (${
-        playlist.songs.length
-      } songs) to queue\n${status(queue)}`
-    )
-  )
+  .on('playSong', (message, queue, song) => {
+    const messageToSend = `Playing \`${song.name}\` - \`${song.formattedDuration}\`\nRequested by: ${song.user.tag}`;
+    message.channel.send(messageToSend);
+  })
+
+  .on('addSong', (message, queue, song) => {
+    const messageToSend = `Added ${song.name} - \`${song.formattedDuration}\` to the queue by ${song.user.tag}`;
+    message.channel.send(messageToSend);
+  })
+
+  .on('playList', (message, queue, playlist, song) => {
+    const messageToSend = `Play \`${playlist.name}\` playlist (${
+      playlist.songs.length
+    } songs).\nRequested by: ${song.user.tag}\nNow playing \`${song.name}\` - \`${
+      song.formattedDuration
+    }\`\n${status(queue)}`;
+    
+    return message.channel.send(messageToSend);
+  })
+
+  .on('addList', (message, queue, playlist) => {
+    const messageToSend = `Added \`${playlist.name}\` playlist (${
+      playlist.songs.length
+    } songs) to queue\n${status(queue)}`;
+    
+    return message.channel.send(messageToSend);
+  })
+
   // DisTubeOptions.searchSongs = true
   .on('searchResult', (message, result) => {
     let i = 0;
-    message.channel.send(
-      `**Choose an option from below**\n${result
+    const messageToSend = `**Choose an option from below**\n${result
         .map(song => `**${++i}**. ${song.name} - \`${song.formattedDuration}\``)
-        .join('\n')}\n*Enter anything else or wait 60 seconds to cancel*`
-    );
+        .join('\n')}\n*Enter anything else or wait 60 seconds to cancel*`;
+    return message.channel.send(messageToSend);
   })
+
   // DisTubeOptions.searchSongs = true
   .on('searchCancel', message => message.channel.send(`Searching canceled`))
   .on('error', (message, e) => {
     console.error(e);
-    message.channel.send('An error encountered: ' + e);
+    const messageToSend = 'An error encountered: ' + e;
+    message.channel.send(messageToSend);
   });
 
-var express = require('express');
-var app = express();
-var path = require('path');
+const express = require('express');
+const app = express();
+const path = require('path');
 
 app.use(express.static(__dirname + '/'));
 app.get('*', (req, res) => {
