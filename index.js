48bd90b8c6cc4ee0654bf178cee95a835f069f42
NutnButTreys <tom.fmcbride@gmail.com>
2023-06-07 23:32:27 -0700

    clean up

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
-const { DisTube } = require('distube')
-const Discord = require('discord.js')
+const { DisTube } = require('distube');
+const Discord = require('discord.js');
 const client = new Discord.Client({
   intents: [
     Discord.GatewayIntentBits.Guilds,
@@ -8,7 +8,7 @@ const client = new Discord.Client({
     Discord.GatewayIntentBits.GuildVoiceStates,
     Discord.GatewayIntentBits.MessageContent
   ]
-})
+});
 const fs = require('fs');
 const { SpotifyPlugin } = require('@distube/spotify');
 const { SoundCloudPlugin } = require('@distube/soundcloud');
@@ -26,17 +26,17 @@ client.distube = new DisTube(client, {
     new SoundCloudPlugin(),
     new YtDlpPlugin()
   ]
-})
-client.commands = new Discord.Collection()
-client.aliases = new Discord.Collection()
+});
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
 client.emotes = {
-      "play": "▶️",
-      "stop": "⏹️",
-      "queue": "📄",
-      "success": "☑️",
-      "repeat": "🔁",
-      "error": "❌"
-  }
+    "play": "▶️",
+    "stop": "⏹️",
+    "queue": "📄",
+    "success": "☑️",
+    "repeat": "🔁",
+    "error": "❌"
+};
 
 fs.readdir('./commands/', (err, files) => {
   if (err) return console.log('Could not find any commands!')
@@ -48,35 +48,36 @@ fs.readdir('./commands/', (err, files) => {
     client.commands.set(cmd.name, cmd)
     if (cmd.aliases) cmd.aliases.forEach(alias => client.aliases.set(alias, cmd.name))
   })
-})
+});
 
 client.on('ready', () => {
-  console.log(`${client.user.tag} is ready to play music.`)
-})
+  console.log(`${client.user.tag} is ready to play music.`);
+});
 
 client.on('messageCreate', async message => {
-  if (message.author.bot || !message.guild) return
-  const prefix = process.env.prefix
-  if (!message.content.startsWith(prefix)) return
-  const args = message.content.slice(prefix.length).trim().split(/ +/g)
-  const command = args.shift().toLowerCase()
-  const cmd = client.commands.get(command) || client.commands.get(client.aliases.get(command))
-  if (!cmd) return
+  if (message.author.bot || !message.guild) return;
+  const prefix = process.env.prefix;
+  if (!message.content.startsWith(prefix)) return;
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  const cmd = client.commands.get(command) || client.commands.get(client.aliases.get(command));
+  if (!cmd) return;
   if (cmd.inVoiceChannel && !message.member.voice.channel) {
-    return message.channel.send(`${client.emotes.error} | You must be in a voice channel!`)
+    return message.channel.send(`${client.emotes.error} | You must be in a voice channel!`);
   }
   try {
-    cmd.run(client, message, args)
+    cmd.run(client, message, args);
   } catch (e) {
-    console.error(e)
-    message.channel.send(`${client.emotes.error} | Error: \`${e}\``)
+    console.error(e);
+    message.channel.send(`${client.emotes.error} | Error: \`${e}\``);
   }
-})
+});
 
 const status = queue =>
   `Volume: \`${queue.volume}%\` | Filter: \`${queue.filters.names.join(', ') || 'Off'}\` | Loop: \`${
     queue.repeatMode ? (queue.repeatMode === 2 ? 'All Queue' : 'This Song') : 'Off'
   }\` | Autoplay: \`${queue.autoplay ? 'On' : 'Off'}\``;
+
 client.distube
   .on('playSong', (queue, song) =>
     queue.textChannel.send(
@@ -105,16 +106,16 @@ client.distube
   .on('searchNoResult', (message, query) =>
     message.channel.send(`${client.emotes.error} | No result found for \`${query}\`!`)
   )
-  .on('finish', queue => queue.textChannel.send('Finished!'))
+  .on('finish', queue => queue.textChannel.send('Finished!'));
 
-// var express = require('express');
-// var app = express();
-// var path = require('path');
+var express = require('express');
+var app = express();
+var path = require('path');
 
-// app.use(express.static(__dirname + '/'));
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, './src/index.html'));
-// });
-// app.listen(process.env.PORT || 8080);
+app.use(express.static(__dirname + '/'));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './src/index.html'));
+});
+app.listen(process.env.PORT || 8080);
 
 client.login(process.env.TOKEN);
